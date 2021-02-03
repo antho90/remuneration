843f98d1f9fb67e5adf953bf3153118eae9cb5dd
Anthony Chatelot <anthonychxtelxt@gmail.com>
2021-02-03 14:08:11 +0100

    custom page le secteur prive

---
+++ page-le-secteur-prive.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div class="container">
+    <div class="row">
+        <h1 class="col-12 text-center fs-title"><?= the_title(); ?></h1>
+        <div class="col-sm-offset-2 col-sm-8 offset-md-2 text-justify text-center">
+            <?php the_content(); ?>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>

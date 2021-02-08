802d86fbc354758e16dcb72ae9aa55aad8dda096
Anthony Chatelot <anthonychxtelxt@gmail.com>
2021-02-08 14:46:26 +0100

    maj footer

--- footer.php
+++ footer.php
@@ -1,14 +1,17 @@
 <footer>
     <p class="mg-foot">© 2021 Anthony Chatelot - Tous droits réservés</p>
 
-      <!-- ajout de ma nouvelle widget area -->
-  <?php if ( is_active_sidebar( 'new-widget-area' ) ) : ?>
- <div id="header-widget-area" class="nwa-header-widget widget-area mg-left" role="complementary">
- <?php dynamic_sidebar( 'new-widget-area' ); ?>
- </div>
- <?php endif; ?>
- <!-- fin nouvelle widget area -->
+    <div class="col-12">
+        <!-- ajout de ma nouvelle widget area -->
+        <?php if (is_active_sidebar('new-widget-area')) : ?>
+            <div id="header-widget-area" class="nwa-header-widget widget-area mg-left" role="complementary">
+                <?php dynamic_sidebar('new-widget-area'); ?>
+            </div>
+        <?php endif; ?>
+        <!-- fin nouvelle widget area -->
+    </div>
 
 </footer>
 </body>
+
 </html>
